d2ebd7706613d9e9bd20adefcdd8431c63cab9a1
vdemirov <demirov.vlad@gmail.com>
2023-10-16 20:37:21 +0800

    Header + Hero section completed

---
+++ components/Navbar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+import Button from "@/components/Button";
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="Hilink" width={74} height={29} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer
+            transition-all hover:underline hover:text-gray-90"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
